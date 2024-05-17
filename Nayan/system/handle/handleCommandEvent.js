968497f18f24ce8958e0b97952bc8a7647fd48e1
LUCIFER-7D <143206799+LUCIFER-7D@users.noreply.github.com>
2024-05-18 01:00:27 +0600

    Update handleCommandEvent.js

--- Nayan/system/handle/handleCommandEvent.js
+++ Nayan/system/handle/handleCommandEvent.js
@@ -1,7 +1,7 @@
 module.exports = function ({ api, models, Users, Threads, Currencies }) {
     const logger = require("../../catalogs/Senayelc.js")
     return function ({ event }) {
-        const { allowInbox } = global.Nayan;
+        const { allowInbox } = global.Senayel;
         const { userBanned, threadBanned } = global.data;
         const { commands, eventRegistered } = global.client;
         var { senderID, threadID } = event;
