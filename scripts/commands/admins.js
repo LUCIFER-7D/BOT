880b2478e24fe080ef24e4f0d4ee634b2f6a7dfc
LUCIFER-7D <143206799+LUCIFER-7D@users.noreply.github.com>
2024-05-18 09:46:14 +0600

    Update admins.js

--- scripts/commands/admins.js
+++ scripts/commands/admins.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: 'admins',
     version: '1.0.0',
     permission: 0,
-    credits: 'nayan',
+    credits: 'senayel',
     prefix: false,
     description: 'group administrator list.',
     category: 'without prefix',
