130d1e930cf969d553bbd53e691885bf1a2cec83
LUCIFER-7D <143206799+LUCIFER-7D@users.noreply.github.com>
2024-05-18 10:12:26 +0600

    Update bangla.js

--- scripts/commands/bangla.js
+++ scripts/commands/bangla.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "bangla",
     version: "1.0.1",
     permission: 0,
-    credits: "Nayan",
+    credits: "Senayel",
     prefix: true,
     description: "ask any thing",
     category: "admin",
