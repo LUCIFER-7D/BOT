36323e72ef1895239e923a7d39d9ccb5b986c3a2
LUCIFER-7D <143206799+LUCIFER-7D@users.noreply.github.com>
2024-05-18 10:28:55 +0600

    Update dictionary.js

--- scripts/commands/dictionary.js
+++ scripts/commands/dictionary.js
@@ -4,7 +4,7 @@ module.exports.config = {
   version: "1.0.0",
   permssion: 0,
   prefix: true,
-  credits: "nayan",
+  credits: "senayel",
   description: "",
   usage: "[text]",
   category: "study",
